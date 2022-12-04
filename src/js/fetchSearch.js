855e2916b97839d8a3c77809b32f9a2778e6f8b8
Anton Shabalov <shabalovanton@gmail.com>
2022-12-04 11:37:32 +0200

    css+

--- src/js/fetchSearch.js
+++ src/js/fetchSearch.js
@@ -4,8 +4,8 @@ export function fetchImag(what, pageGallery, resultsPerPage) {
   const URL = 'https://pixabay.com/api';
   return axios.get(URL, {
     params: {
-      q: what,
       key: '31737650-012dbd0b1d73fc9a5bf6ca0f4',
+      q: what,
       type: 'photo',
       orientation: 'horizontal',
       safesearch: 'true',

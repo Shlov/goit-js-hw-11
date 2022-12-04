4f25f235f1390b5935dae01ca6562ee2861ccf62
Anton Shabalov <shabalovanton@gmail.com>
2022-12-04 14:20:37 +0200

    Update fetchSearch.js

--- src/js/fetchSearch.js
+++ src/js/fetchSearch.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export function fetchImag(what, pageGallery, resultsPerPage) {
-  const URL = 'https://pixabay.com/api';
+  const URL = 'https://pixabay.com/api/';
   return axios.get(URL, {
     params: {
       key: '31737650-012dbd0b1d73fc9a5bf6ca0f4',
